40eee7926a912abe6607b884a70f9555bf135a03
Tadej Novak <tadej@tano.si>
2021-05-02 14:32:03 +0200

    Use accent color for links

--- qcocoatheme.mm
+++ qcocoatheme.mm
@@ -146,7 +146,11 @@ static QPalette *qt_mac_createSystemPalette()
 
     palette->setBrush(QPalette::ToolTipBase, qt_mac_toQBrush([NSColor controlColor]));
 
-    palette->setColor(QPalette::Normal, QPalette::Link, qt_mac_toQColor([NSColor linkColor]));
+    if (__builtin_available(macOS 10.14, *)) {
+        palette->setColor(QPalette::Normal, QPalette::Link, qt_mac_toQColor([NSColor controlAccentColor]));
+    } else {
+        palette->setColor(QPalette::Normal, QPalette::Link, qt_mac_toQColor([NSColor linkColor]));
+    }
 
     return palette;
 }
